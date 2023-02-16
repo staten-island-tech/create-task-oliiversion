6169de378761e53b1f2837a7efe69376f0600f84
oliiversion <olivxrkei101@gmail.com>
2023-02-16 12:59:57 -0500

    i have no idea what im doing kinda

--- js/averagescript.js
+++ js/averagescript.js
@@ -1,3 +1,14 @@
+const inputgrades = [
+  { variable: "a", number: input },
+  { variable: "b", number: input },
+  { variable: "c", number: input },
+  { variable: "d", number: input },
+  { variable: "e", number: input },
+  { variable: "f", number: input },
+  { variable: "g", number: input },
+  { variable: "h", number: input },
+];
+
 function average(a, b, c, d, e, f, g, h) {
   if (a > 0 && b > 0 && c > 0 && d > 0 && e > 0 && f > 0 && g > 0 && h > 0) {
     return a + b + c + d + e + f + g + h;
