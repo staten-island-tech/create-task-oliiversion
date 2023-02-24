070cfa4dd6b0d59e50ea4dc045d1b19737fbc7a2
oliiversion <olivxrkei101@gmail.com>
2023-02-23 22:28:14 -0500

    yes

--- js/lettersscript.js
+++ js/lettersscript.js
@@ -1,28 +1,32 @@
-const letters = [
-  { letter: "A", number: [90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100] },
-  { letter: "B", number: [80, 81, 82, 83, 84, 85, 86, 87, 88, 89] },
-  { letter: "C", number: [70, 71, 72, 73, 74, 75, 76, 77, 78, 79] },
-  { letter: "D", number: [65, 66, 67, 68, 69] },
-  { letter: "F", number: x < 65 },
-];
 
-function calculategpa(form) {
-  var gpa = form.gpa2.value;
+let numberGrade = []
 
-  if (gpa == "A") {
-    gpa = 4;
+addEventListener("submit", function(numberGrade) {
+  numberGrade.push(numberGrade)
+})
+
+
+
+
+
+function convert(numberGrade){
+  let letterGrade = undefined;
+
+  if (numberGrade < 65) {
+    letterGrade = "F"
   }
-  if (gpa == "B") {
-    gpa = 3;
-  }
-  if (gpa == "C") {
-    gpa = 2;
-  }
-  if (gpa == "D") {
-    gpa = 1;
-  }
-  if (gpa == "F") {
-    gpa = 0;
-  }
-  document.getElementById("result1").innerHTML = gpa;
+  else if (numberGrade < 70) {
+    letterGrade = "D"}
+  else if (numberGrade < 80) {
+    letterGrade = "C"}
+  else if (numberGrade < 90) {
+    letterGrade = "B"}
+  else if (numberGrade <= 100) {
+    letterGrade = "A"}
+
+    return letterGrade;
 }
+
+
+
+module.exports = {numberGrade, letterGrade}
