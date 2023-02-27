15d4b08bef8a179ee8bbcfe8e38b432022b9f210
oliiversion <olivxrkei101@gmail.com>
2023-02-27 13:10:15 -0500

    im so lost

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
-import letters from "./lettersscript";
-import average from "./averagescript";
+import { letterGrade } from "./lettersscript";
+import { numberGrade } from "./lettersscript";
+import { numberGrade, letterGrade } from "./lettersscript";
+
+
+
+DOMSelectors.makecard.addEventListener("click", function () {
+    let numberGrade = DOMSelectors.songtitle.value;
+    let artistname = DOMSelectors.artistname.value;
+    let image = DOMSelectors.image.value;
