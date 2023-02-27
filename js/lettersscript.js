673673acb919eeb2076edcd4851ebd9884c7fb08
oliiversion <olivxrkei101@gmail.com>
2023-02-27 13:14:00 -0500

    Update lettersscript.js

--- js/lettersscript.js
+++ js/lettersscript.js
@@ -1,32 +1,43 @@
+let numberGrade = [];
 
-let numberGrade = []
+addEventListener("submit", function (numberGrade) {
+  numberGrade.push(numberGrade);
+});
 
-addEventListener("submit", function(numberGrade) {
-  numberGrade.push(numberGrade)
-})
-
-
-
-
-
-function convert(numberGrade){
+function convert(numberGrade) {
   let letterGrade = undefined;
 
   if (numberGrade < 65) {
-    letterGrade = "F"
+    letterGrade = "F";
+  } else if (numberGrade < 70) {
+    letterGrade = "D";
+  } else if (numberGrade < 80) {
+    letterGrade = "C";
+  } else if (numberGrade < 90) {
+    letterGrade = "B";
+  } else if (numberGrade <= 100) {
+    letterGrade = "A";
   }
-  else if (numberGrade < 70) {
-    letterGrade = "D"}
-  else if (numberGrade < 80) {
-    letterGrade = "C"}
-  else if (numberGrade < 90) {
-    letterGrade = "B"}
-  else if (numberGrade <= 100) {
-    letterGrade = "A"}
 
-    return letterGrade;
+  return letterGrade;
 }
 
+function convert(letterGrade) {
+  let letterGrade = undefined;
 
+  if ((letterGrade = "F")) {
+    GPA = "0";
+  } else if (numberGrade < 70) {
+    letterGrade = "D";
+  } else if (numberGrade < 80) {
+    letterGrade = "C";
+  } else if (numberGrade < 90) {
+    letterGrade = "B";
+  } else if (numberGrade <= 100) {
+    letterGrade = "A";
+  }
 
-module.exports = {numberGrade, letterGrade}
+  return letterGrade;
+}
+
+module.exports = { numberGrade, letterGrade };
