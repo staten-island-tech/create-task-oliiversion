2a010be1de2d4e95326df4e51eb0f3437ec3f59d
oliiversion <olivxrkei101@gmail.com>
2023-02-27 13:11:41 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@ import { letterGrade } from "./lettersscript";
 import { numberGrade } from "./lettersscript";
 import { numberGrade, letterGrade } from "./lettersscript";
 
-
-
 DOMSelectors.makecard.addEventListener("click", function () {
-    let numberGrade = DOMSelectors.numberGrade.value;
-    let letterGrade = DOMSelectors.letterGrade.value;
+  let numberGrade = DOMSelectors.numberGrade.value;
+  let letterGrade = DOMSelectors.letterGrade.value;
+});
